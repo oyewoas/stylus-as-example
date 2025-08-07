3b1402744bb8d949be47a0ff58e5a25ada79ec26
oyewoas <ayooluwaoyewo@gmail.com>
2025-08-07 13:48:07 +0100

    Modified app.ts main funtion to use getMinEvenPrime

--- assembly/app.ts
+++ assembly/app.ts
@@ -1,80 +1,84 @@
 import { i32ToBytes, bytesToI32 } from './utils';
 
 // Unsigned square root
-function usqrt(n: u32): u32 {
-  let x = n;
-  let y = (x + 1) >> 1;
-  while (y < x) {
-    x = y;
-    y = (x + n / x) >> 1;
-  }
-  return x;
-}
+// function usqrt(n: u32): u32 {
+//   let x = n;
+//   let y = (x + 1) >> 1;
+//   while (y < x) {
+//     x = y;
+//     y = (x + n / x) >> 1;
+//   }
+//   return x;
+// }
 
 /**
  * Returns the max prime below or equal a given "number" using the Sieve of Eratosthenes algorithm
  * (Based on t-katsumura's implementation: https://github.com/t-katsumura/webassembly-examples-eratosthenes)
  * (Sieve of Eratosthenes explanation: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes)
  */
-function getMaxPrimeUpTo(number: i32): i32 {
-  // 0 here is meant to be interpreted as an error
-  if (number < 2) {
-    return 0;
-  }
+// function getMaxPrimeUpTo(number: i32): i32 {
+//   // 0 here is meant to be interpreted as an error
+//   if (number < 2) {
+//     return 0;
+//   }
 
-  // If 2 is passed, return 2
-  if (number == 2) {
-    return 2;
-  }
+//   // If 2 is passed, return 2
+//   if (number == 2) {
+//     return 2;
+//   }
 
-  // Length of the sieve array
-  const length = (number - 1) / 2;
+//   // Length of the sieve array
+//   const length = (number - 1) / 2;
 
-  // Square root (max)
-  const maxNumberToCheck = usqrt(number);
+//   // Square root (max)
+//   const maxNumberToCheck = usqrt(number);
 
-  // Sieve array (starting from 3, without multiples of 2) [3, 5, 7, 9, ...]
-  const sieve = new StaticArray<bool>(length).fill(true);
+//   // Sieve array (starting from 3, without multiples of 2) [3, 5, 7, 9, ...]
+//   const sieve = new StaticArray<bool>(length).fill(true);
 
-  // Coordinates of the Sieve
-  let x: u32;
-  let y: u32;
+//   // Coordinates of the Sieve
+//   let x: u32;
+//   let y: u32;
 
-  for (let i = 0; i < length; i++) {
-    // Next number to check multiples for
-    x = 2 * (i + 1) + 1;
+//   for (let i = 0; i < length; i++) {
+//     // Next number to check multiples for
+//     x = 2 * (i + 1) + 1;
 
-    // No need to check multiples for numbers that are greater than
-    // the square root of the upper limit
-    if (x > maxNumberToCheck) {
-      break;
-    }
+//     // No need to check multiples for numbers that are greater than
+//     // the square root of the upper limit
+//     if (x > maxNumberToCheck) {
+//       break;
+//     }
 
-    // Checking multiples of x
-    for (let j = i + 1; j < length; j++) {
-      if (!unchecked(sieve[j])) {
-        continue;
-      }
+//     // Checking multiples of x
+//     for (let j = i + 1; j < length; j++) {
+//       if (!unchecked(sieve[j])) {
+//         continue;
+//       }
 
-      // Next multiple candidate
-      y = 2 * (j + 1) + 1;
+//       // Next multiple candidate
+//       y = 2 * (j + 1) + 1;
 
-      // Candidate is multiple of x (not prime then)
-      if (y % x == 0) {
-        unchecked((sieve[j] = false));
-      }
-    }
-  }
+//       // Candidate is multiple of x (not prime then)
+//       if (y % x == 0) {
+//         unchecked((sieve[j] = false));
+//       }
+//     }
+//   }
 
-  // Get the highest prime number
-  let max_val: u32 = 2;
-  for (let i = length - 1; i >= 0; i--) {
-    if (unchecked(sieve[i])) {
-      max_val = 2 * (i + 1) + 1;
-      break;
-    }
-  }
-  return max_val;
+//   // Get the highest prime number
+//   let max_val: u32 = 2;
+//   for (let i = length - 1; i >= 0; i--) {
+//     if (unchecked(sieve[i])) {
+//       max_val = 2 * (i + 1) + 1;
+//       break;
+//     }
+//   }
+//   return max_val;
+// }
+
+function getMinEvenPrime(n: i32): i32 {
+  return n < 2 ? 0 : 2;
 }
 
 /**
@@ -86,6 +90,6 @@ function getMaxPrimeUpTo(number: i32): i32 {
  */
 export const main = (input: Uint8Array): Uint8Array => {
   const maxNumber = bytesToI32(input);
-  const maxPrime = getMaxPrimeUpTo(maxNumber);
-  return i32ToBytes(maxPrime);
+  const minEvenPrime = getMinEvenPrime(maxNumber);
+  return i32ToBytes(minEvenPrime);
 };
